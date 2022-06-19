c17843aa77658a746c5b44f2da206ab37556931b
Sarah Tumenas <sarah.marques@spread.com.br>
2022-06-19 10:28:50 -0300

    Implementando uma funcionalidade

---
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -0,0 +1,25 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+
+import java.math.BigDecimal;
+
+public class ReajusteService {
+    public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
+        if(desempenho == Desempenho.A_DESEJAR) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+            funcionario.reajustarSalario(reajuste);
+        }
+
+        if(desempenho == Desempenho.Bom) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
+            funcionario.reajustarSalario(reajuste);
+        }
+
+        if(desempenho == Desempenho.Otimo) {
+            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.2"));
+            funcionario.reajustarSalario(reajuste);
+        }
+    }
+}
